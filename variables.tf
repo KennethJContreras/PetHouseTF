e67f821a93a2e96eeeaeade0a1b83f5c328b99dd
Your Name <kenneth.contreras@unah.hn>
2024-07-28 11:52:34 -0600

    Configurando terraform

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "project"{
+    description = "the project name"
+    default = "pethouse"
+}
+
+variable "environment"{
+    description = "the enviroment to release"
+    default = "dev"
+}
+
+variable "location"{
+    description = "Azure region"
+    default = "East Us 2"
+}
+
+variable "tags"{
+    description = "all tags used"
+    default = {
+        environment = "dev"
+        project = "pethouse"
+        created_by = "terraform"
+    }
+}
+
+variable "password"{
+    description = "sqlserver password"
+    type = string
+    sensitive = true
+}
+
